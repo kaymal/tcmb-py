4c9b0ee1a9176f5a7cc0c08ad398cd5e2be24b75
Kaymal <gutkyle@gmail.com>
2023-04-21 02:08:49 +0200

    add standalone reader function

--- tcmb/core.py
+++ tcmb/core.py
@@ -13,13 +13,131 @@ from __future__ import annotations
 from datetime import date
 from functools import cached_property
 import os
-import re
 
 import pandas as pd
-import numpy as np
-import requests
 
-from tcmb import const, auth, utils
+from tcmb import const, auth, utils, fetch
+
+
+def read(
+    series: str | list[str],
+    start: str | None = None,
+    end: str | None = None,
+    agg: str | list[str] | None = None,
+    formulas: str | None = None,
+    freq: str | int | None = None,
+    seperator: str = ".",
+    headers: dict | None = None,
+    api_key: str | None = None,
+    **kwargs,
+) -> pd.DataFrame:
+    """Read data from TCMB's EVDS Web Service.
+
+    Parameters
+    ----------
+    series:
+        Time series key. e.g. "TP.DK.USD.S.YTL"
+    start: str or datetime-like
+        Start date of the series in DD-MM-YYYY, YYYY-MM-DD, or datetime-like
+        format.
+    end: str or datetime-like
+        End date of the series in DD-MM-YYYY, YYYY-MM-DD, or datetime-like
+        format.
+    agg: {None, "avg", "min", "max", "first", "last", "sum"}
+        Aggregation type of the result. If type of the `series` is string,
+        type of the `agg` must also be string. If `series` is a list,
+        `agg` can be either a `str` or a `list`.
+    formulas:
+        Compute values such as percentage change, difference,
+        year-over-year (YoY) chanhe, Year-to-date (YTD) change,
+        moving average.
+        - 0: level
+        - 1: percentage change
+        - 2: difference
+        - 3: YoY percentage change
+        - 4: YoY difference
+        - 5: YTD percentage change
+        - 6: YTD difference
+        - 7: moving average
+        - 8: moving sum
+    freq:
+        Frequency of the series. Either frequency string or int to be used.
+        - None: the default frequency of the series is taken.
+        - "D": 1,      # daily
+        - "B": 2,      # business
+        - "W-FRI": 3,  # weekly (friday)
+        - "W": 3,      # weekly (friday)
+        - "M": 5,      # monthly
+        - "Q": 6,      # quarterly
+        - "2Q": 7,     # semi-annual
+        - "A": 8,      # annual
+        - "Y": 8,      # annual
+    seperator:
+        Delimiter to use.
+    api_key:
+        API key of the user. If None, environment variable with
+        the name "TCMB_API_KEY" will be used to as the api_key. If
+        the api_key argument is not passed or it is not exported to the
+        environment, ApiKeyError is raised.
+    **kwargs:
+        Keyword arguments passed to the request.
+
+    Returns
+    -------
+    df: Pandas DataFrame.
+
+    References
+    ----------
+    - https://evds2.tcmb.gov.tr/help/videos/EVDS_Web_Service_Usage_Guide.pdf
+
+    Example
+    -------
+    import tcmb
+
+    df = tcmb.read(["...", "..."])
+
+    """
+    api_key = api_key or os.environ.get("TCMB_API_KEY")
+
+    # convert freq string to
+    if isinstance(freq, str):
+        freq = const.FREQ_MAPPING[freq]
+
+    # convert list to str seperated by "-"
+    # as descibed in the api reference
+    if isinstance(series, list):
+        series = "-".join(series)
+
+    # convert list to str seperated by "-"
+    if isinstance(agg, list):
+        agg = "-".join(agg)
+
+    # convert date string formats to "DD-MM-YYYY"
+    if start:
+        start = utils.standardize_date(start)
+    if end:
+        end = utils.standardize_date(end)
+
+    params = {
+        "series": series,
+        "startDate": start or "01-01-1970",
+        "endDate": end or date.today().strftime("%d-%m-%Y"),
+        "type": "json",  # csv, xml, json
+        "aggregationTypes": agg,
+        "key": api_key,
+        "formulas": formulas,
+        "frequency": freq,
+        "decimalSeperator": seperator,
+        **kwargs,
+    }
+
+    res = fetch.get_response(params=params, headers=headers)
+
+    # convert response JSON to DataFrame
+    #   time series data is in the "items"
+    data = utils.to_dataframe(res.json()["items"])
+
+    return data
 
 
 class Client:
@@ -30,52 +148,16 @@ class Client:
     api_key:
         API key of the user. If None, environment variable with
         the name "TCMB_API_KEY" will be used to as the api_key. If
-        the key argument is not passed or it is not exported to the
+        the api_key argument is not passed or it is not exported to the
         environment, ApiKeyError is raised.
     """
-
-    URL = "https://evds2.tcmb.gov.tr/service/evds"
 
     def __init__(self, api_key: str | None = None):
         self.api_key = api_key or os.environ.get("TCMB_API_KEY")
         auth.check_api_key(self.api_key)
 
-        self.today_str = date.today().strftime("%d-%m-%Y")
-
-    def _create_uri(self, params: dict, endpoint: str | None = None) -> str:
-        """Create URI using the params and the evds url.
-
-        The official api does not use `?` for query strings.
-        Therefore the parameters are added to the end of the url
-        instead of passing to the requests.get() function as `params`.
-
-        Parameters
-        ----------
-        params:
-            Query parameters.
-        endpoint:
-            One of
-            - "categories"
-            - "serieList"
-            - "datagroups"
-            - None
-        """
-        if endpoint is not None:
-            endpoint = endpoint + "/"
-        else:
-            endpoint = ""
-
-        # create query string
-        # NOTE: alternative -> urllib.parse.urlencode(params)
-        #       however, this keeps None values
-        query_str = "&".join(
-            [k + "=" + str(v) for k, v in params.items() if v is not None]
-        )
-
-        return f"{Client.URL}/{endpoint}{query_str}"
-
+    @staticmethod
     def _get_response(
-        self,
         params: dict,
         headers: dict | None = None,
         endpoint: str | None = None,
@@ -102,51 +184,15 @@ class Client:
         kwargs:
             Optional keyword arguments that request takes.
         """
-        # Create url for the get request
-        #  NOTE: the official api does not use ? for query strings
-        #  Therefore the parameters are added to the end of the url
-        #  instead of passing to the get request as params
-        url = self._create_uri(params=params, endpoint=endpoint)
-
-        # Get request
-        res = requests.get(url, headers=headers, proxies=proxies, **kwargs)
-
-        # Check status
-        res.raise_for_status()
+        res = fetch.get_response(
+            params=params,
+            headers=headers,
+            endpoint=endpoint,
+            proxies=proxies,
+            **kwargs,
+        )
 
         return res
-
-    @staticmethod
-    def _to_dataframe(response):
-        """Convert json response to pandas DataFrame."""
-        df = pd.DataFrame(response.json()["items"])
-        # drop unused column
-        df = df.drop("UNIXTIME", axis=1)
-        # set date as index
-        # TODO: check if "Tarih" always the first column
-        df = df.set_index(df.columns[0])
-
-        # detect date format
-        if re.match(r"\d+-\d+-\d{4}", df.index[0]):
-            date_format = "%d-%m-%Y"
-        elif re.match(r"\d+-\d{4}", df.index[0]):
-            date_format = "%m-%Y"
-        elif re.match(r"\d{4}-\d+", df.index[0]):
-            date_format = "%Y-%m"
-        elif re.match(r"\d{4}", df.index[0]):
-            date_format = "%Y"
-        # convert date strings to datetime
-        df.index = pd.to_datetime(df.index, format=date_format)
-
-        # replace None with NaN
-        df = df.fillna(np.nan)
-        # convert object columns to float
-        # TODO: convert to integer when possible
-        df = df.astype(float)
-        # drop rows if all missing
-        df = df.dropna(how="all")
-
-        return df
 
     def read(
         self,
@@ -224,46 +270,18 @@ class Client:
         -------
         df = client.read(["...", "..."])
         """
-        # convert freq string to
-        if isinstance(freq, str):
-            freq = const.FREQ_MAPPING[freq]
-
-        # convert list to str seperated by "-"
-        # as descibed in the api reference
-        if isinstance(series, list):
-            series = "-".join(series)
-
-        # convert list to str seperated by "-"
-        if isinstance(agg, list):
-            agg = "-".join(agg)
-
-        # convert date string formats to "DD-MM-YYYY"
-        if start:
-            start = utils.standardize_date(start)
-        if end:
-            end = utils.standardize_date(end)
-
-        params = {
-            "series": series or "TP.DK.USD.S.YTL",
-            "startDate": start or "01-01-1970",
-            "endDate": end or self.today_str,
-            "type": "json",  # csv, xml, json
-            "aggregationTypes": agg,
-            "key": self.api_key,
-            "formulas": formulas,
-            "frequency": freq,
-            "decimalSeperator": seperator,
+        df = read(
+            series=series,
+            start=start,
+            end=end,
+            agg=agg,
+            formulas=formulas,
+            freq=freq,
+            seperator=seperator,
+            headers=headers,
+            api_key=self.api_key,
             **kwargs,
-        }
-
-        res = self._get_response(params=params, headers=headers)
-
-        # Convert to DataFrame
-        try:
-            df = self._to_dataframe(res)
-        except Exception as err:
-            err.with_traceback(None)
-            return res.json()
+        )
 
         # if metadata is true, save metadata info
         # to the .attrs attribute of the DataFrame
@@ -384,4 +402,4 @@ class Client:
         return self.get_datagroups_metadata()
 
     def __repr__(self) -> str:
-        return "tcmb Client instance"
+        return "tcmb client"
--- tcmb/utils.py
+++ tcmb/utils.py
@@ -2,6 +2,9 @@
 import re
 from datetime import datetime
 
+import numpy as np
+import pandas as pd
+
 
 def standardize_date(date_str: str) -> str:
     """Standardize date string format to output DD-MM-YYYY.
@@ -35,3 +38,35 @@ def standardize_date(date_str: str) -> str:
         date_format = "%Y.%m.%d"
 
     return datetime.strptime(date_str, date_format).strftime("%d-%m-%Y")
+
+
+def to_dataframe(data: dict) -> pd.DataFrame:
+    """Convert data from the json response to pandas DataFrame."""
+    df = pd.DataFrame(data)
+    # drop unused column
+    df = df.drop("UNIXTIME", axis=1)
+    # set date as index
+    # TODO: check if "Tarih" always the first column
+    df = df.set_index(df.columns[0])
+
+    # detect date format
+    if re.match(r"\d+-\d+-\d{4}", df.index[0]):
+        date_format = "%d-%m-%Y"
+    elif re.match(r"\d+-\d{4}", df.index[0]):
+        date_format = "%m-%Y"
+    elif re.match(r"\d{4}-\d+", df.index[0]):
+        date_format = "%Y-%m"
+    elif re.match(r"\d{4}", df.index[0]):
+        date_format = "%Y"
+    # convert date strings to datetime
+    df.index = pd.to_datetime(df.index, format=date_format)
+
+    # replace None with NaN
+    df = df.fillna(np.nan)
+    # convert object columns to float
+    # TODO: convert to integer when possible
+    df = df.astype(float)
+    # drop rows if all missing
+    df = df.dropna(how="all")
+
+    return df
